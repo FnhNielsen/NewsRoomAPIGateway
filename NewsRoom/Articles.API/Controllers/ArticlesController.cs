a78410317e933c48482692a6bd58373126fbf71c
Frederik Nielsen <frederik@icsharp.io>
2022-09-23 16:58:15 +0200

    added article services

--- NewsRoom/Articles.API/Controllers/ArticlesController.cs
+++ NewsRoom/Articles.API/Controllers/ArticlesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Articles.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,40 @@ namespace Articles.API.Controllers
     [ApiController]
     public class ArticlesController : ControllerBase
     {
-        
+        private readonly ArticleService _articleService;
+        public ArticlesController(ArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_articleService.GetAll());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetArticle(int id)
+        {
+            var article = _articleService.GetArticle(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(article);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteArticle(int id)
+        {
+            var articleId = _articleService.Delete(id);
+            if (articleId == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
     }
 }
--- NewsRoom/Articles.API/Services/ArticleService.cs
+++ NewsRoom/Articles.API/Services/ArticleService.cs
@@ -4,18 +4,54 @@ namespace Articles.API.Services;
 
 public class ArticleService : IArticleService
 {
+    private readonly static List<Article> _articles = Populate();
+    private static List<Article> Populate()
+    {
+        var result = new List<Article>()
+        {
+            new Article()
+            {
+                Id = 1,
+                Title = "First title",
+                Created = DateTime.Today,
+                LastUpdate = DateTime.Now,
+                WriterId = 2
+            },
+            new Article
+            {
+                Id = 2,
+                Title = "Second title",
+                Created = DateTime.Today,
+                LastUpdate = DateTime.Now,
+                WriterId = 2
+            },
+            new Article
+            {
+                Id = 3,
+                Title = "Third title",
+                Created = DateTime.Today,
+                LastUpdate = DateTime.Now,
+                WriterId = 1
+            }
+        };
+        return result;
+    }
+
     public List<Article> GetAll()
     {
-        throw new NotImplementedException();
+        return _articles;
     }
 
     public Article GetArticle(int id)
     {
-        throw new NotImplementedException();
+        return _articles.FirstOrDefault(a => a.Id == id) ?? throw new InvalidOperationException();
     }
 
     public int Delete(int id)
     {
-        throw new NotImplementedException();
+        var removed = _articles.SingleOrDefault(x => x.Id == id);
+        if (removed != null)
+            _articles.Remove(removed);
+        return removed?.Id ?? 0;
     }
 }
